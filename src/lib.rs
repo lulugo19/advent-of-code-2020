277201f736b06f76bfdefd6fcc7d42ab3285b9ff
Lukas Gobelet <lulugo19@web.de>
2020-12-19 15:57:07 +0100

    day 19 solved

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate aoc_runner_derive;
 
 #[macro_use]
 extern crate lazy_static;
+extern crate itertools;
 extern crate regex;
 
 pub mod day1;
@@ -19,6 +20,7 @@ pub mod day15;
 pub mod day16;
 pub mod day17;
 pub mod day18;
+pub mod day19;
 pub mod day2;
 pub mod day3;
 pub mod day4;
