da8903ba038b1bb9c6a37faf5e403105d6c9d481
Lukas Gobelet <lulugo19@web.de>
2020-12-23 11:25:08 +0100

    renamed days

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,15 @@ extern crate lazy_static;
 extern crate itertools;
 extern crate regex;
 
-pub mod day1;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
+pub mod day08;
+pub mod day09;
 pub mod day10;
 pub mod day11;
 pub mod day12;
@@ -21,17 +29,9 @@ pub mod day16;
 pub mod day17;
 pub mod day18;
 pub mod day19;
-pub mod day2;
 pub mod day20;
 pub mod day21;
 pub mod day22;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
 
 #[derive(Debug)]
 pub struct NoSolutionErr {}
